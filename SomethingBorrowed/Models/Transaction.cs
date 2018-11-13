aa44ecaf7decce589feabbc12c340368ff46a5a7
Zack <44037441+zackjenkinsIS@users.noreply.github.com>
2018-11-12 20:06:29 -0700

    i did this sorry

---
+++ SomethingBorrowed/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SomethingBorrowed.Models
+{
+    public class Transaction
+    {
+        /*[Table("Transaction")]*/
+        [Key]
+        [Required(ErrorMessage = "Error")]
+        public int transactionID { get; set; }
+        /* [ForeignKey]*/
+        [Required(ErrorMessage = "Error")]
+        public int dressID { get; set; }
+        [Required(ErrorMessage = "Error")]
+        public string transactionDate { get; set; }
+        [Required(ErrorMessage = "Error")]
+        public int price { get; set; }
+        [Required(ErrorMessage = "Error")]
+        public string startRentalDate { get; set; }
+        [Required(ErrorMessage = "Error")]
+        public string endRentalDate { get; set; }
+    }
+}
