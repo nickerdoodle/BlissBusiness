37d6aa64e3045c354769e14e578568f181a5664d
nmmahe <nicholasmahe@gmail.com>
2018-11-12 18:42:00 -0700

    dress view

---
+++ SomethingBorrowed/Controllers/DressController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SomethingBorrowed.Controllers
+{
+    public class DressController : Controller
+    {
+        // GET: Dress
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
