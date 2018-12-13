821f539a46aa2e07fb16f8976820e96ba7f6f231
cnelson27 <cnelson27@outlook.com>
2018-12-13 13:53:12 -0700

    Added Owner Information and layout

--- SomethingBorrowed/Controllers/OwnerController.cs
+++ SomethingBorrowed/Controllers/OwnerController.cs
@@ -1,7 +1,10 @@
 ﻿using SomethingBorrowed.DAL;
+using SomethingBorrowed.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,5 +19,99 @@ namespace SomethingBorrowed.Controllers
         {
             return View(db.Owners.ToList());
         }
+
+        public ActionResult Create()
+        {
+            ViewBag.StateID = new SelectList(db.States, "StateID", "StateDesc");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Owner owner)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Owners.Add(owner);
+                db.SaveChanges();
+                int id = owner.OwnerID;
+                ViewBag.OwnerName = owner.OwnerFirstName + " " + owner.OwnerLastName;
+                return View("ConfirmNewOwner");
+            }
+            ViewBag.StateID = new SelectList(db.States, "StateID", "StateDesc");
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            
+            ViewBag.StateID = new SelectList(db.States, "StateID", "StateDesc");
+            return View(owner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Owner owner)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Entry(owner).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(owner);
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Owner owner = db.Owners.Find(id);
+            db.Owners.Remove(owner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: Compounds/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Owner owner = db.Owners.Find(id);
+            if (owner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(owner);
+        }
+    
     }
 }
