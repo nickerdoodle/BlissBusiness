59f80eae63afd40cccf0d02312460df50b089d50
cnelson27 <cnelson27@outlook.com>
2018-12-12 17:50:58 -0700

    added entity to global asax

--- SomethingBorrowed/Global.asax.cs
+++ SomethingBorrowed/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SomethingBorrowed.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +14,8 @@ namespace SomethingBorrowed
     {
         protected void Application_Start()
         {
+            Database.SetInitializer<BridalContext>(null);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
